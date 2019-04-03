5d95724da3bbb423e475adf96ad4f0dac1cbc064
Kostyal Balint <kostyal.balintnak@gmail.com>
2019-04-03 09:00:46 +0200

    Login Form Argon Design

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html class="h-100" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -25,100 +25,47 @@
     <link type="text/css" href="/assets/css/argon.css?v=1.0.0" rel="stylesheet">
 
 </head>
-<body>
-    <div id="app">
-      <main class="bg-default">
-        <nav class="navbar navbar-expand-md navbar-light navbar-laravel" style="border: 1px solid red;">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @auth
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Kijelentkezés') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endauth
-                    </ul>
-                </div>
-            </div>
-        </nav>
+<body class="h-100">
+      <main class="h-100 bg-default main-content">
 
         <!-- Top navbar -->
-        <nav class="navbar navbar-top navbar-expand-md navbar-dark" id="navbar-main">
-          <div class="container-fluid">
+        <nav class="navbar navbar-top navbar-horizontal navbar-expand-md navbar-dark">
+          <div class="container px-4">
             <!-- Brand -->
             <a class="h4 mb-0 text-white text-uppercase d-lg-inline-block" href="{{ url('/') }}">{{ config('app.name', 'Laravel') }}</a>
             <!-- User -->
             <ul class="navbar-nav align-items-center d-lg-inline-block">
+              @auth
               <li class="nav-item dropdown">
                 <a class="text-white pr-0" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                   <div class="media align-items-center">
                     <div class="media-body ml-2 ">
-                      <span class="mb-0 text-sm  font-weight-bold">Misnyovszki Benjámin Balázs</span>
+                      <span class="mb-0 text-sm  font-weight-bold">{{ Auth::user()->name }}</span>
                     </div>
                   </div>
                 </a>
                 <div class="dropdown-menu dropdown-menu-arrow dropdown-menu-right">
-                  <div class=" dropdown-header noti-title">
-                    <h6 class="text-overflow m-0">Welcome!</h6>
-                  </div>
-                  <a href="../examples/profile.html" class="dropdown-item">
-                    <i class="ni ni-single-02"></i>
-                    <span>My profile</span>
+                  <a class="dropdown-item" href="{{ route('logout') }}"
+                     onclick="event.preventDefault();
+                                   document.getElementById('logout-form').submit();">
+                      <i class="ni ni-user-run"></i>
+                      {{ __('Kijelentkezés') }}
                   </a>
-                  <a href="../examples/profile.html" class="dropdown-item">
-                    <i class="ni ni-settings-gear-65"></i>
-                    <span>Settings</span>
-                  </a>
-                  <a href="../examples/profile.html" class="dropdown-item">
-                    <i class="ni ni-calendar-grid-58"></i>
-                    <span>Activity</span>
-                  </a>
-                  <a href="../examples/profile.html" class="dropdown-item">
-                    <i class="ni ni-support-16"></i>
-                    <span>Support</span>
-                  </a>
-                  <div class="dropdown-divider"></div>
-                  <a href="#!" class="dropdown-item">
-                    <i class="ni ni-user-run"></i>
-                    <span>Logout</span>
-                  </a>
+
+                  <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                      @csrf
+                  </form>
                 </div>
               </li>
+              @endauth
             </ul>
           </div>
         </nav>
 
+
             @yield('content')
         </main>
-    </div>
+
 
     <!-- Argon Scripts -->
     <!-- Core -->
