13c038f40b1a0b4d19d9dafe4ae51d01d70207bf
Kostyal Balint <kostyal.balintnak@gmail.com>
2019-04-05 11:02:02 +0200

    wip

--- resources/views/admin/import.blade.php
+++ resources/views/admin/import.blade.php
@@ -1,11 +1,14 @@
 @extends('admin.template')
 
 @section('content')
-  <form class="" action="{{ route('admin.import') }}" method="post">
+  <form class="" action="{{ route('admin.import') }}" method="POST">
     @csrf
+    <p>Diákok</p>
     <input type="file" name="students" value="">
+    <p>Tanárok</p>
     <input type="file" name="teachers" value="">
+    <p>Csoportok</p>
     <input type="file" name="groups" value="">
-    <input type="submit" name="" value="">
+    <input type="submit" name="" value="submint">
   </form>
 @endsection
