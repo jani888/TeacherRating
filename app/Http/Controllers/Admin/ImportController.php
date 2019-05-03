b53df83068794f2373a8030a0d3e97874140eccf
Hidvégi János <jani.hidvegi@gmail.com>
2019-05-03 09:58:11 +0200

    Update ImportController.php

--- app/Http/Controllers/Admin/ImportController.php
+++ app/Http/Controllers/Admin/ImportController.php
@@ -9,13 +9,13 @@ use App\Imports\StudentGroupsImport;
 use App\Imports\TeachersImport;
 use App\Imports\UsersImport;
 use App\Models\Group;
-use App\Models\ImportProgress;
 use App\Models\Rating;
 use App\Models\SchoolClass;
 use App\Models\Teacher;
 use App\User;
 use Illuminate\Console\OutputStyle;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Output\ConsoleOutput;
 
@@ -26,24 +26,21 @@ class ImportController extends Controller {
     }
 
     public function store(Request $request) {
-        $validator = \Validator::make(
-            [
-                'students'      => $request->students,
-                'students_extension' => strtolower($request->students->getClientOriginalExtension()),
-                'teachers'      => $request->teachers,
-                'teachers_extension' => strtolower($request->teachers->getClientOriginalExtension()),
-                'groups'      => $request->groups,
-                'groups_extension' => strtolower($request->groups->getClientOriginalExtension()),
-            ],
-            [
-                'students'          => 'required',
-                'students_extension'      => 'required|in:xlsx,xls',
-                'teachers'          => 'required',
-                'teachers_extension'      => 'required|in:xlsx,xls',
-                'groups'          => 'required',
-                'groups_extension'      => 'required|in:xlsx,xls',
-            ]
-        );
+        $validator = \Validator::make([
+            'students'           => $request->students,
+            'students_extension' => strtolower($request->students->getClientOriginalExtension()),
+            'teachers'           => $request->teachers,
+            'teachers_extension' => strtolower($request->teachers->getClientOriginalExtension()),
+            'groups'             => $request->groups,
+            'groups_extension'   => strtolower($request->groups->getClientOriginalExtension()),
+        ], [
+            'students'           => 'required',
+            'students_extension' => 'required|in:xlsx,xls',
+            'teachers'           => 'required',
+            'teachers_extension' => 'required|in:xlsx,xls',
+            'groups'             => 'required',
+            'groups_extension'   => 'required|in:xlsx,xls',
+        ]);
         $validator->validate();
         User::truncate();
         Teacher::truncate();
@@ -53,43 +50,35 @@ class ImportController extends Controller {
         \DB::table('group_teacher')->truncate();
         \DB::table('group_user')->truncate();
         ini_set('max_execution_time', 30000);
-        //Start a new import progress (progress: 0-5 -> 0-100%)
-        $importProgress = ImportProgress::create();
-
         $paths = [
             "students" => $request->file('students')->storeAs('imports', 'students.xslx'),
             "teachers" => $request->file('teachers')->storeAs('imports', 'teachers.xslx'),
             "groups"   => $request->file('groups')->storeAs('imports', 'groups.xslx'),
         ];
 
-        (new SchoolClassesImport)->withOutput(new OutputStyle(new ArrayInput([]), new ConsoleOutput()))->import($paths["students"]);
-        (new UsersImport)->withOutput(new OutputStyle(new ArrayInput([]), new ConsoleOutput()))->import($paths["students"]);
-        (new TeachersImport)->withOutput(new OutputStyle(new ArrayInput([]), new ConsoleOutput()))->import($paths["teachers"]);
-        (new GroupsImport)->withOutput(new OutputStyle(new ArrayInput([]), new ConsoleOutput()))->import($paths["teachers"]);
-        (new StudentGroupsImport)->withOutput(new OutputStyle(new ArrayInput([]), new ConsoleOutput()))->import($paths["groups"]);
+        $fakeOutput = new OutputStyle(new ArrayInput([]), new ConsoleOutput());
+        (new SchoolClassesImport)->withOutput($fakeOutput)->import($paths["students"]);
+        (new UsersImport)->withOutput($fakeOutput)->import($paths["students"]);
+        (new TeachersImport)->withOutput($fakeOutput)->import($paths["teachers"]);
+        (new GroupsImport)->withOutput($fakeOutput)->import($paths["teachers"]);
+        (new StudentGroupsImport)->withOutput($fakeOutput)->import($paths["groups"]);
 
-        /*//Import classes
-        SchoolClassesImportJob::withChain([
-            //Imports all students
-            UsersImportJob::withChain([
-                new UpdateProgressJob($importProgress),
-            ])->dispatch($paths['students']),
-            //Imports all teachers
-            TeachersImportJob::withChain([
-                new UpdateProgressJob($importProgress),
-            ])->dispatch($paths['teachers']),
-            //Imports teacher groups
-            GroupsImportJob::withChain([
-                new UpdateProgressJob($importProgress),
-            ])->dispatch($paths['teachers']),
-            //Imports student groups
-            StudentGroupsImportJob::withChain([
-                new UpdateProgressJob($importProgress),
-            ])->dispatch($paths['groups']),
-            new UpdateProgressJob($importProgress),
-        ])->dispatch($paths['students']);
-        session()->flash('importID', $importProgress->id);*/
-        session()->flash('status', 'success');
-        return back();
+        //Count all inserted rows by table
+        $count = [];
+        $count[] = DB::table('users')->count();
+        $count[] = DB::table('teachers')->count();
+        $count[] = DB::table('school_classes')->count();
+        $count[] = DB::table('groups')->count();
+        $count[] = DB::table('group_user')->count();
+        $count[] = DB::table('group_teacher')->count();
+        //filter tables with no rows
+        $noRowsInserted = array_filter($count, function ($table) {
+            return $table == 0;
+        });
+        if (count($noRowsInserted) > 0) {
+            return 'warning';
+        }
+
+        return 'success';
     }
 }
