c60d18cc2b0369f992318e1bf3232e896bd8a9a0
Kostyal Balint <kostyal.balintnak@gmail.com>
2020-02-21 11:57:08 +0100

    group filter

--- resources/views/admin/groups.blade.php
+++ resources/views/admin/groups.blade.php
@@ -13,10 +13,26 @@
                     <h3 class="mb-0">Csoportok aktiválása</h3>
                 </div>
                 <div class="card-body">
-                    <div class="row text-right">
-                        <div class="col-md-3 float-right">
-                            <div class="form-group pl-3">
-                                <input class="form-control small" type="text" placeholder="Szűrés" id="filter-group-name">
+                    <div class="row d-flex align-items-center pl-3 mb-3">
+                        <div class="col-md-3">
+                            <div class="form-group mb-0">
+                                <input class="form-control small" type="text" placeholder="Csoprotok szűrése" id="filter-group-name">
+                            </div>
+                        </div>
+                        <div class="col-md-9">
+                            <div class="form-group mb-0">
+                                <div class="custom-control custom-radio custom-control-inline">
+                                    <input type="radio" id="filter-radio-begins" name="filter-radio" class="custom-control-input">
+                                    <label class="custom-control-label" for="filter-radio-begins">Elején</label>
+                                </div>
+                                <div class="custom-control custom-radio custom-control-inline">
+                                    <input type="radio" id="filter-radio-ends" name="filter-radio" class="custom-control-input">
+                                    <label class="custom-control-label" for="filter-radio-ends">Végén</label>
+                                </div>
+                                <div class="custom-control custom-radio custom-control-inline">
+                                    <input type="radio" id="filter-radio-contains" name="filter-radio" class="custom-control-input" checked>
+                                    <label class="custom-control-label" for="filter-radio-contains">Tartalmazza</label>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -33,7 +49,7 @@
                                     <td>{{$group->id}}</td>
                                     <td class="name">{{ $group->name }}</td>
                                     <td>
-                                        <label class="custom-toggle">
+                                        <label class="custom-toggle mb-0">
                                             <input class="custom-control-input" id="active_{{ $group->id }}" type="checkbox" name="active[{{ $group->id }}]" @if(true) checked @endif>
                                             <span class="custom-toggle-slider rounded-circle"></span>
                                         </label>
@@ -53,12 +69,25 @@
     <script>
       $(document).ready(function(){
         $("#filter-group-name").on("keyup", function() {
-          var value = $(this).val().toLowerCase();
-          $("#results-table tr.group-row").filter(function() {
-            console.log($(this).children(".name").text()); //todo: group filtering
-            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-          });
+            filter();
+        });
+        $("input[name=filter-radio]").on("click", function() {
+          filter();
         });
       });
+
+      function filter(){
+        var value = $("#filter-group-name").val().toLowerCase();
+        $("#results-table tr.group-row").filter(function() {
+          let text = $(this).children(".name").text();
+          if($("input#filter-radio-begins").prop("checked")){
+            $(this).toggle(text.startsWith(value));
+          }else if($("input#filter-radio-ends").prop("checked")){
+            $(this).toggle(text.endsWith(value));
+          }else if($("input#filter-radio-contains").prop("checked")){
+            $(this).toggle(text.indexOf(value) > -1)
+          }
+      });
+      }
     </script>
 @endpush
