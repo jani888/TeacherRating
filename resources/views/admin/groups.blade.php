d44a80f9945d2d6f977b36bb4e9fcbcc8754ca8d
Kostyal Balint <kostyal.balintnak@gmail.com>
2020-04-21 14:14:26 +0200

    Update groups.blade.php

--- resources/views/admin/groups.blade.php
+++ resources/views/admin/groups.blade.php
@@ -36,6 +36,7 @@
                                 <div class="">
                                     <a id="all-on" class="btn btn-success text-white"> A szűrt-ek bekapcsolása</a>
                                     <a id="all-off" class="btn btn-warning text-white"> A szűrt-ek kikapcsolása</a>
+                                    <button onclick='$("#save").click()' class="btn btn-primary">Mentés</button>
                                 </div>
                             </div>
                         </div>
@@ -67,7 +68,7 @@
                             </table>
                         </div>
 
-                        <button type="submit" class="btn btn-primary">Mentés</button>
+                        <button type="submit" id="save" class="btn btn-primary mt-3">Mentés</button>
                     </form>
                 </div>
             </div>
@@ -100,20 +101,6 @@
             });
         });
 
-        $("#all-on").click(() => {
-          //$("#results-table tr.group-row:visible").children().prop( "checked", true );
-          $("#results-table tr.group-row:visible").each(function() {
-            $(this).find("input[type='checkbox']").prop("checked", true);
-          });
-        });
-        $("#all-off").click(() => {
-          //$("#results-table tr.group-row:visible").children().prop( "checked", true );
-          $("#results-table tr.group-row:visible").each(function() {
-            $(this).find("input[type='checkbox']").prop("checked", false);
-          });
-        });
-      });
-
       function filter(){
         var value = $("#filter-group-name").val().toLowerCase();
         $("#results-table tr.group-row").filter(function() {
@@ -136,9 +123,9 @@
 
 
         /* Create 'click' event handler for rows */
-        rows.on('click', function(e) {
+        rows.on('click', function (e) {
 
-          if ($(e.target).is('.custom-toggle-slider') || $(e.target).is('.custom-control-input')){
+          if($(e.target).is('.custom-toggle-slider') || $(e.target).is('.custom-control-input')) {
             e.stopPropagation();
             return;
           }
@@ -149,7 +136,7 @@
           /* Check if 'Ctrl', 'cmd' or 'Shift' keyboard key was pressed
            * 'Ctrl' => is represented by 'e.ctrlKey' or 'e.metaKey'
            * 'Shift' => is represented by 'e.shiftKey' */
-          if ((e.ctrlKey || e.metaKey) || e.shiftKey) {
+          if((e.ctrlKey || e.metaKey) || e.shiftKey) {
             /* If pressed highlight the other row that was clicked */
             //row.addClass('highlight');
           } else {
@@ -157,52 +144,8 @@
             //rows.removeClass('highlight');
             //row.addClass('highlight');
           }
-
-        //Selecting rows from the table
-        $(document).ready(function () {
-
-            /* Get all rows from your 'table' but not the first one
-             * that includes headers. */
-            var rows = $('tr').not(':first');
-
-
-            /* Create 'click' event handler for rows */
-            rows.on('click', function (e) {
-
-                if ($(e.target).is('.custom-toggle-slider') || $(e.target).is('.custom-control-input')) {
-                    e.stopPropagation();
-                    return;
-                }
-
-                /* Get current row */
-                var row = $(this);
-
-                /* Check if 'Ctrl', 'cmd' or 'Shift' keyboard key was pressed
-                 * 'Ctrl' => is represented by 'e.ctrlKey' or 'e.metaKey'
-                 * 'Shift' => is represented by 'e.shiftKey' */
-                if ((e.ctrlKey || e.metaKey) || e.shiftKey) {
-                    /* If pressed highlight the other row that was clicked */
-                    //row.addClass('highlight');
-                } else {
-                    /* Otherwise just highlight one row and clean others */
-                    //rows.removeClass('highlight');
-                    //row.addClass('highlight');
-                }
-
-            });
-
-            /* This 'event' is used just to avoid that the table text
-             * gets selected (just for styling).
-             * For example, when pressing 'Shift' keyboard key and clicking
-             * (without this 'event') the text of the 'table' will be selected.
-             * You can remove it if you want, I just tested this in
-             * Chrome v30.0.1599.69 */
-            $(document).bind('selectstart dragstart', function (e) {
-                e.preventDefault();
-                return false;
-            });
-
         });
+      });
 
     </script>
 @endpush
